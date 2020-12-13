f1b27ecb577924367e15f574ef41ee67191c3aa9
Emily Waters <emily.waters@arm.com>
2020-12-13 10:30:40 +0000

    adding code to train best model over all species

--- models/train_best_all.py
+++ models/train_best_all.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 import os
 from models.data_setup import data_generators
 import models.mymodels_configurable as mymodelsconfig
-from models.train_mymodels import fit_evaluate_model
+import pickle
+from results.generate_metrics import get_metrics
 
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -20,10 +21,20 @@ def vary_class_number():
     for j in range(1, 79):
         classesj, train_data_genj, val_data_genj, test_data_genj = data_generators(j, batch_size=batch_size,
                                                                                    IMG_DIM=IMG_DIM)
-        model = mymodelsconfig.vgg6(IMG_DIM, j, 'adamax', 'he_normal')
-
-        fit_evaluate_model(model, 'best_model', 'best_' + str(j))
-        model(train_data_genj, val_data_genj, test_data_genj, num_classes=classesj, IMG_DIM=IMG_DIM, epochs=epochs)
+        model = mymodelsconfig.vgg6(IMG_DIM, classesj, 'adamax', 'he_normal')
+        callbacks = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)]
+        model.fit(
+            train_data_genj,
+            epochs=epochs,
+            validation_data=val_data_genj,
+            callbacks=callbacks)
+        eval = model.evaluate(test_data_genj, verbose=0)
+        # store prediction stats
+        results = get_metrics(model, test_data_genj)
+        results['accuracy'] = eval[1]
+        results['loss'] = eval[0]
+        with open('best_model/model' + str(classesj) + '.txt', 'wb') as file_pi:
+            pickle.dump(results, file_pi)
 
 
 vary_class_number()
